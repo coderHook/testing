cbd9fa983cae6f3c83a77cf79546c5289ea7721c
coderhook <coderhookdev@gmail.com>
2021-12-08 20:36:55 +0100

    Add index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const { parse } = require("csv-parse");
+
+const assetsWithIntrinsicVal = [];
+
+function isHabitable(planet) {
+  return (
+    planet["koi_insol"] > 0.36 && planet["koi_disposition"] === "CONFIRMED"
+  );
+}
+const test = fs
+  .createReadStream("./solar_data.csv")
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
+  .on("data", (data) => {
+    if (isHabitable(data)) {
+      habitablePlanets.push(data);
+    }
+  })
+  .on("error", (err) => {
+    console.error(err);
+  })
+  .on("end", () => {
+    console.log("FOUND: ", habitablePlanets.length);
+  });
+
+console.log({ test });
